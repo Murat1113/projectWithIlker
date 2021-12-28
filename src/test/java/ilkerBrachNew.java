ce1465b0157c5dc00d04ce1c3b6a13c8eed4563c
ilker <mehmetilker51@gmail.com>
2021-12-28 15:43:16 +0100

    new branch 03

--- src/test/java/ilkerBrachNew.java
+++ src/test/java/ilkerBrachNew.java
@@ -2,5 +2,6 @@ public class ilkerBrachNew {
     public static void main(String[] args) {
         System.out.println("new 1");
         System.out.println("new 2");
+        System.out.println("new 3");
     }
 }
